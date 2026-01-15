22c8e5ac14e2d4d1af2901eeeb6154e12bdc89ff
Nyan Lin Aung <150507886+blaze-nyan@users.noreply.github.com>
2026-01-15 13:50:18 +0700

    removed contact phone number

--- components/MenuBackground.tsx
+++ components/MenuBackground.tsx
@@ -23,49 +23,6 @@ export default function MenuBackground({
         }}
       />
 
-      {/* Texture overlays */}
-      <div className="absolute inset-0">
-        {/* Metal 1 overlay */}
-        <div className="absolute inset-0 mix-blend-soft-light opacity-80">
-          <Image
-            src="/background-assets/metal-overlay-bg.webp"
-            alt=""
-            fill
-            sizes="100vw"
-            className="object-cover pointer-events-none"
-            priority
-          />
-        </div>
-
-        {/* Grunge overlay */}
-        <div className="absolute flex inset-0 items-center justify-center mix-blend-soft-light">
-          <div className="flex-none h-full scale-y-[-100%] w-full">
-            <div className="opacity-30 relative size-full">
-              <Image
-                src="/background-assets/grunge-overlay-bg.webp"
-                alt=""
-                fill
-                sizes="100vw"
-                className="object-cover pointer-events-none"
-              />
-            </div>
-          </div>
-        </div>
-
-        {/* Gradient overlay */}
-        <div className="absolute flex inset-0 items-center justify-center mix-blend-soft-light">
-          <div className="flex-none h-full rotate-[180deg] scale-y-[-100%] w-full">
-            <div
-              className="opacity-80 size-full"
-              style={{
-                backgroundImage:
-                  "linear-gradient(rgba(128, 128, 128, 0.6) 0%, rgb(128, 128, 128) 19.684%, rgba(128, 128, 128, 0.3) 70.46%, rgb(128, 128, 128) 100%)",
-              }}
-            />
-          </div>
-        </div>
-      </div>
-
       {/* Content */}
       {children && (
         <div className="relative z-10 w-full h-full">{children}</div>
@@ -73,4 +30,3 @@ export default function MenuBackground({
     </div>
   );
 }
-
